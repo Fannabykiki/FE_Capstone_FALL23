cb52c6231f71590c458f2def3b39a3966e8b9e87
Hung Thanh <hungthanh15022001@gmail.com>
2023-10-14 14:40:53 +0700

    List_CreateProject

--- src/pages/User/Project/views/Conponents/CardProject.js
+++ src/pages/User/Project/views/Conponents/CardProject.js
@@ -1,28 +1,44 @@
-
-import React from 'react'
-
-import TaskCreateProject from './TaskCreateProject';
+import React, { useState, useEffect } from 'react';
 import { Card, Space } from 'antd';
+import axios from 'axios';
+
 const CardProject = () => {
+  const [projectCard, setProjectCard] = useState([]);
 
-    return (
-        <>
+  useEffect(() => {
+    const getProjects = async () => {
+      try {
+        const userId = JSON.parse(decodeURIComponent(sessionStorage.userId));
+        
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/project-management/projects/${userId}`);
+        setProjectCard(response.data);
+        console.log(response);
+      } catch (error) {
+        console.error('Error fetching project data:', error);
+      }
+    };
 
-            <Space direction="vertical" size={16}>
-                <Card
-                    title="ProjectName"
-                    extra={<a href="#">More</a>}
-                    style={{
-                        width: 300, backgroundColor: '#eee8e8', margin: '10px', display: 'block'
-                    }}
-                >
-                    <p>Description</p>
+    getProjects();
+  }, []);
 
-                </Card>
+  if (projectCard.length === 0) {
+    return <div>You don't have any project.</div>;
+  }
 
-            </Space>
+  return (
+    <Space direction="vertical" size={16}>
+      {projectCard.map((project) => (
+        <Card
+          title={project.projectName}
+          extra={<a href="#">More</a>}
+          style={{ width: 300, backgroundColor: '#eee8e8', margin: '10px', display: 'block' }}
+        >
+          <p>Description: {project.description}</p>
+          <p>Privacy Status: {project.privacyStatus ? ' Public' : ' Private'}</p>
+        </Card>
+      ))}
+    </Space>
+  );
+};
 
-        </>
-    )
-}
 export default CardProject;
