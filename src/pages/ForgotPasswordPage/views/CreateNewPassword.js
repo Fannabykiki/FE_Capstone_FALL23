8bb9b23d60539c22f4a60176bea4ce334bb0bc58
Hung Thanh <hungthanh15022001@gmail.com>
2023-10-18 01:41:53 +0700

    m

--- src/pages/ForgotPasswordPage/views/CreateNewPassword.js
+++ src/pages/ForgotPasswordPage/views/CreateNewPassword.js
@@ -1,101 +1,136 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { MailOutlined } from "@ant-design/icons";
-// import { Button, Form, Input, Typography } from "antd";
-// import ReCAPTCHA from "react-google-recaptcha";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { MailOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Typography } from "antd";
+import ReCAPTCHA from "react-google-recaptcha";
 
-// import logo from "../../../assets/images/Devtask.png";
+import logo from "../../../assets/images/Devtask.png";
 
-// const { Text, Link } = Typography;
+const { Text, Link } = Typography;
 
-// const CreateNewPassword = () => {
-//     const [form] = Form.useForm();
-//     const [isRecaptchaVerified, setIsRecaptchaVerified] = useState(false);
-//     const [email, setEmail] = useState("");
-//     const [resetPasswordStatus, setResetPasswordStatus] = useState(null);
+const CreateNewPassword = () => {
+    const [form] = Form.useForm();
+ 
+    const [email, setEmail] = useState("");
+    const [resetPasswordStatus, setResetPasswordStatus] = useState(null);
 
-//     useEffect(() => {
-//         const link = window.location.href;
-//         const queryParams = link.split("?");
-//         const token = queryParams[1];
-//         const decodedEmail = decodeURIComponent(localStorage.email);
-//         const email = JSON.parse(decodedEmail);
-//         setEmail(email);
+    useEffect(() => {
+        const link = window.location.href;
+        const queryParams = link.split("?");
+        const token = queryParams[1];
+        const Email = decodeURIComponent(localStorage.email);
 
-//         const verifyResetPwd = async () => {
-//             try {
-//                 const response = await axios.post(
-//                     `${process.env.REACT_APP_API_URL}/api/authentication/forgot-password?email=${email}&verifyToken=${token}`
-//                 );
-//                 setResetPasswordStatus(response);
-//             } catch (error) {
-//                 console.log("Error:", error);
-//             }
-//         };
+        setEmail(Email);
 
-//         verifyResetPwd();
-//     }, []);
+        const verifyResetPwd = async () => {
+            try {
+                const response = await axios.post(
+                    `${process.env.REACT_APP_API_URL}/api/authentication/forgot-password?email=${Email}&verifyToken=${token}`
+                );
+                setResetPasswordStatus(response);
+                console.log(Email);
+            } catch (error) {
+                console.log("Error:", error);
+            }
+        };
 
-//     const handleForgotPassword = () => {
-//         // Handle the logic for resetting the password
-//     };
+        verifyResetPwd();
+    }, []);
 
-//     return (
-//         <div className="forgotpassword">
-//             <div className="Form-forgotpassword">
-//                 <div className="Logo">
-//                     <img className="ImageLogo" src={logo} alt="Logo" />
-//                 </div>
-//                 <Form form={form} className="container" layout="vertical">
-//                     <Form.Item
-//                         label={<b>Email</b>}
-//                         name="email"
-//                         rules={[
-//                             {
-//                                 whitespace: true,
-//                                 required: true,
-//                                 message: "Email is required",
-//                             },
-//                         ]}
-//                         validateTrigger={["onBlur", "onChange"]}
-//                     >
-//                         <Input
-//                             size="large"
-//                             prefix={<MailOutlined />}
-//                             onChange={(e) => setEmail(e.target.value)}
-//                         />
-//                     </Form.Item>
+    const handleForgotPassword = async () => {
+        try {
+            const values = await form.validateFields();
+            const { newPwd, ConfirmnewPwd } = values;
 
-//                     <Form.Item>
-//                         <ReCAPTCHA
-//                             className="btncapcha"
-//                             onChange={(value) => setIsRecaptchaVerified(value)}
-//                             style={{ justifyContent: "center" }}
-//                             hl="en"
-//                             sitekey={process.env.REACT_APP_SITE_KEY}
-//                         />
-//                     </Form.Item>
+            if (newPwd !== ConfirmnewPwd) {
+                // Xử lý khi mật khẩu mới và xác nhận mật khẩu không khớp
+                return;
+            }
 
-//                     <Form.Item>
-//                         <Button
-//                             className="custombtn"
-//                             block
-//                             type="primary"
-//                             onClick={handleForgotPassword}
-//                             disabled={!isRecaptchaVerified}
-//                         >
-//                             <Text className="customtxt">Reset Password</Text>
-//                         </Button>
-//                     </Form.Item>
+            const link = window.location.href;
+            const queryParams = link.split("?");
+            const token = queryParams[1];
+            const email = decodeURIComponent(localStorage.email);
 
-//                     <Form.Item style={{ textAlign: "center" }}>
-//                         <Text>Already have an account?</Text>
-//                         <Link href="/login">Sign in</Link>
-//                     </Form.Item>
-//                 </Form>
-//             </div>
-//         </div>
-//     );
-// };
+            const response = await axios.post(
+                `${process.env.REACT_APP_API_URL}/api/authentication/reset-password`,
+                {
+                    email,
+                    token,
+                    newPassword: newPwd,
+                    confirmPassword: ConfirmnewPwd,
+                }
+            );
 
-// export default CreateNewPassword;
+            // Xử lý khi reset mật khẩu thành công
+            setResetPasswordStatus(response);
+
+        } catch (error) {
+            // Xử lý khi có lỗi xảy ra
+            console.log("Error:", error);
+        }
+    };
+
+    return (
+        <div className="forgotpassword">
+            <div className="Form-forgotpassword">
+                <div className="Logo">
+                    <img className="ImageLogo" src={logo} alt="Logo" />
+                </div>
+                <Form form={form} className="container" layout="vertical">
+                    {resetPasswordStatus === 200 && (
+                        <>
+                            <Form.Item
+                                label={<b>New Password</b>}
+                                name="newPwd"
+                                rules={[
+                                    {
+                                        whitespace: true,
+                                        required: true,
+                                        message: "Password is required",
+                                    },
+                                ]}
+                                validateTrigger={["onBlur", "onChange"]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item
+                                label={<b>Confirm New Password</b>}
+                                name="ConfirmnewPwd"
+                                rules={[
+                                    {
+                                        whitespace: true,
+                                        required: true,
+                                        message: "Password is required",
+                                    },
+                                ]}
+                                validateTrigger={["onBlur", "onChange"]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                        </>
+                    )}
+
+
+                    <Form.Item>
+                        <Button
+                            className="custombtn"
+                            block
+                            type="primary"
+                            onClick={handleForgotPassword}
+                        >
+                            <Text className="customtxt">Reset Password</Text>
+                        </Button>
+                    </Form.Item>
+
+                    <Form.Item style={{ textAlign: "center" }}>
+                        <Text>Already have an account?</Text>
+                        <Link href="/login">Sign in</Link>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default CreateNewPassword;
