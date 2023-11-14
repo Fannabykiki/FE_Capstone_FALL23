b1460bfb730dac748a3eb3d5e8bc2c55e37f1fc8
sonnh2605 <nguyenhoaison260501@gmail.com>
2023-11-14 23:42:45 +0700

    Integrate invite member modal

--- src/features/Project/Detail/index.tsx
+++ src/features/Project/Detail/index.tsx
@@ -3,7 +3,7 @@ import useProjectDetail from "@/hooks/useProjectDetail";
 import { EProjectPrivacyStatusLabel, IProject } from "@/interfaces/project";
 import { paths } from "@/routers/paths";
 import { Avatar, Button, Tooltip, Typography } from "antd";
-import { useEffect, useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { faker } from "@faker-js/faker";
 import {
@@ -14,8 +14,12 @@ import {
   UnlockOutlined,
 } from "@ant-design/icons";
 import { toast } from "react-toastify";
+import InviteMemberModal from "./InviteMemberModal";
 
 export default function ProjectDetail() {
+  const [isOpenInviteMemberModal, setOpenInviteMemberModal] =
+    useState<boolean>(false);
+
   const { projectId } = useParams();
   const { userInfo } = useAuthContext();
   const navigate = useNavigate();
@@ -38,12 +42,12 @@ export default function ProjectDetail() {
         projects = JSON.parse(savedProjectsString) as Partial<IProject>[];
       }
       projects = projects.filter(
-        (project) => project.projectId !== detail.projectId
+        (project) => project.projectId !== detail?.projectId
       );
       projects.unshift({
-        projectId: detail.projectId,
-        projectName: detail.projectName,
-        description: detail.description,
+        projectId: detail?.projectId,
+        projectName: detail?.projectName,
+        description: detail?.description,
       });
       localStorage.setItem(
         `${userInfo?.id}-projects`,
@@ -52,11 +56,16 @@ export default function ProjectDetail() {
     }
   }, [detail, userInfo]);
 
-  const onUpdatePrivacyStatus = (_detail: IProject) => {
+  const handleOpenInviteMemberModal = () => {
+    setOpenInviteMemberModal(true);
+  };
+
+  const onUpdatePrivacyStatus = (_detail: IProject | undefined) => {
+    if (!_detail) return;
     actions.updatePrivacyStatus(
       {
-        id: _detail.projectId,
-        privacyStatus: !_detail.privacyStatus,
+        id: _detail?.projectId,
+        privacyStatus: !_detail?.privacyStatus,
       },
       {
         onSuccess: (_, variables) => {
@@ -73,80 +82,83 @@ export default function ProjectDetail() {
     );
   };
 
-  if (detail) {
-    return (
-      <>
-        <div className="flex justify-between items-center">
-          <Typography.Title>{detail.projectName}</Typography.Title>
-          <div className="flex gap-x-4">
-            <Button
-              icon={
-                detail.privacyStatus ? <UnlockOutlined /> : <LockOutlined />
-              }
-              type={detail.privacyStatus ? "primary" : "default"}
-              loading={actions.isUpdatingPrivacyStatus}
-              onClick={() => onUpdatePrivacyStatus(detail)}
-            >
-              {detail.privacyStatus
-                ? EProjectPrivacyStatusLabel.Public
-                : EProjectPrivacyStatusLabel.Private}
-            </Button>
-            <Button type="primary" icon={<PlusOutlined />}>
-              Invite
-            </Button>
-          </div>
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <Typography.Title>{detail?.projectName}</Typography.Title>
+        <div className="flex gap-x-4">
+          <Button
+            icon={detail?.privacyStatus ? <UnlockOutlined /> : <LockOutlined />}
+            type={detail?.privacyStatus ? "primary" : "default"}
+            loading={actions.isUpdatingPrivacyStatus}
+            onClick={() => onUpdatePrivacyStatus(detail)}
+          >
+            {detail?.privacyStatus
+              ? EProjectPrivacyStatusLabel.Public
+              : EProjectPrivacyStatusLabel.Private}
+          </Button>
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+            onClick={handleOpenInviteMemberModal}
+          >
+            Invite
+          </Button>
         </div>
-        <div className="flex gap-4">
-          <div className="bg-white shadow p-4 flex-grow h-fit">
-            <p className="font-semibold text-xl">About this project</p>
-            <Typography.Paragraph>{detail.description}</Typography.Paragraph>
-          </div>
-          <div className="basis-1/3 flex-shrink-0 flex flex-col gap-4">
-            <div className="bg-white shadow p-4">
-              <p className="font-semibold text-xl">Project Stats</p>
-              <p className="font-semibold text-lg">Boards</p>
-              <div className="flex gap-x-4">
-                <div className="basis-1/2 flex gap-x-2 items-center">
-                  <div>
-                    <FileAddOutlined className="text-2xl" />
-                  </div>
-                  <div>
-                    <div>0</div>
-                    <div>Work items created</div>
-                  </div>
+      </div>
+      <div className="flex gap-4">
+        <div className="bg-white shadow p-4 flex-grow h-fit">
+          <p className="font-semibold text-xl">About this project</p>
+          <Typography.Paragraph>{detail?.description}</Typography.Paragraph>
+        </div>
+        <div className="basis-1/3 flex-shrink-0 flex flex-col gap-4">
+          <div className="bg-white shadow p-4">
+            <p className="font-semibold text-xl">Project Stats</p>
+            <p className="font-semibold text-lg">Boards</p>
+            <div className="flex gap-x-4">
+              <div className="basis-1/2 flex gap-x-2 items-center">
+                <div>
+                  <FileAddOutlined className="text-2xl" />
                 </div>
-                <div className="basis-1/2 flex gap-x-2 items-center">
-                  <div>
-                    <FileDoneOutlined className="text-2xl" />
-                  </div>
-                  <div>
-                    <div>0</div>
-                    <div>Work items completed</div>
-                  </div>
+                <div>
+                  <div>{detail?.totalTaskCreated}</div>
+                  <div>Task created</div>
+                </div>
+              </div>
+              <div className="basis-1/2 flex gap-x-2 items-center">
+                <div>
+                  <FileDoneOutlined className="text-2xl" />
+                </div>
+                <div>
+                  <div>{detail?.totalTaskCompleted}</div>
+                  <div>Task completed</div>
                 </div>
               </div>
             </div>
-            <div className="bg-white shadow p-4">
-              <p className="font-semibold text-xl">Members</p>
-              <Avatar.Group maxCount={8}>
-                {detail.projectMembers.map((member) => (
-                  <Tooltip
-                    key={member.memberId}
-                    title={member.fullname}
-                    placement="top"
-                  >
-                    <Avatar
-                      key={member.userId}
-                      src={faker.image.avatarGitHub()}
-                    />
-                  </Tooltip>
-                ))}
-              </Avatar.Group>
-            </div>
+          </div>
+          <div className="bg-white shadow p-4">
+            <p className="font-semibold text-xl">Members</p>
+            <Avatar.Group maxCount={4}>
+              {detail?.projectMembers.map((member) => (
+                <Tooltip
+                  key={member.memberId}
+                  title={member.fullname}
+                  placement="top"
+                >
+                  <Avatar
+                    key={member.userId}
+                    src={faker.image.avatarGitHub()}
+                  />
+                </Tooltip>
+              ))}
+            </Avatar.Group>
           </div>
         </div>
-      </>
-    );
-  }
-  return <></>;
+        <InviteMemberModal
+          isOpen={isOpenInviteMemberModal}
+          handleClose={() => setOpenInviteMemberModal(false)}
+        />
+      </div>
+    </>
+  );
 }
