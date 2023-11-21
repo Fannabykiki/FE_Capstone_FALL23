52e8cf6fbabe04c80ef9e490b217f4cce6013619
NgHuuDuc <ducnhhe153066@fpt.edu.vn>
2023-11-22 01:08:49 +0700

    feat: allow editing sprint name and date

--- src/components/Iteration/DisplayName.tsx
+++ src/components/Iteration/DisplayName.tsx
@@ -1,6 +1,10 @@
 import { IIteration } from "@/interfaces/iteration";
+import { iterationApi } from "@/utils/api/iteration";
+import { useMutation } from "@tanstack/react-query";
 import { Input } from "antd";
+import { pick } from "lodash";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 interface Props {
   iteration: IIteration;
@@ -9,6 +13,35 @@ interface Props {
 export default function DisplayName({ iteration }: Props) {
   const [isEditing, setIsEditing] = useState(false);
   const [newName, setNewName] = useState(iteration.interationName);
+
+  const { mutate: updateIteration } = useMutation({
+    mutationKey: [iterationApi.updateKey],
+    mutationFn: iterationApi.update,
+    onSettled: () => setIsEditing(false),
+    onSuccess: (err) => {
+      console.error(err);
+      toast.error("Update iteration name failed! Please try again later");
+      setNewName(iteration.interationName);
+    },
+  });
+
+  const onUpdateName = () => {
+    if (newName && newName !== iteration.interationName) {
+      const newIterationData = pick(iteration, [
+        "interationId",
+        "startDate",
+        "endDate",
+        "statusId",
+      ]);
+      updateIteration({
+        id: iteration.interationId,
+        data: { ...newIterationData, interationName: newName },
+      });
+    } else {
+      setNewName(iteration.interationName);
+      setIsEditing(false);
+    }
+  };
 
   if (!isEditing) {
     return (
@@ -22,8 +55,9 @@ export default function DisplayName({ iteration }: Props) {
   }
   return (
     <Input
+      autoFocus
       className="w-[500px]"
-      onBlur={() => setIsEditing(false)}
+      onBlur={onUpdateName}
       value={newName}
       onChange={(e) => setNewName(e.target.value)}
     />
