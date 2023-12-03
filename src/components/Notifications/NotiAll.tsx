54a62d061477b4953a68247d69919d6d7f0c4e32
NgHuuDuc <ducnhhe153066@fpt.edu.vn>
2023-12-04 00:49:09 +0700

    fix: noti display

--- src/components/Notifications/NotiAll.tsx
+++ src/components/Notifications/NotiAll.tsx
@@ -1,48 +1,45 @@
 import CircleIcon from "@/assets/icons/iconCircle";
 import { INotification } from "@/interfaces/notification";
 import { classNames } from "@/utils/common";
-import { List, Typography } from "antd";
+import { Typography } from "antd";
 import dayjs from "dayjs";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface Props {
   notifications: INotification[];
 }
 
 export default function NotiAll({ notifications }: Props) {
-  const navigate = useNavigate();
-
   return (
-    <List
-      itemLayout="horizontal"
-      dataSource={notifications}
-      renderItem={(item, index) => (
-        <List.Item
-          className="hover:bg-neutral-200 p-2"
-          onClick={() => navigate(item.targetUrl)}
-        >
-          <List.Item.Meta
-            title={
-              <div className="flex items-center">
-                <Typography.Link
-                  className={classNames(
-                    "flex-grow",
-                    !item.isRead && "text-[#a0a3a7]"
-                  )}
-                >
-                  {item.title}
-                </Typography.Link>
-                {!item.isRead && (
-                  <div>
-                    <CircleIcon style={{ fontSize: "12px" }} />
-                  </div>
-                )}
+    <>
+      <div className="max-h-[300px] overflow-y-auto flex flex-col gap-y-2">
+        {notifications.map((noti) => (
+          <Link key={noti.notificationId} to={noti.targetUrl}>
+            <div
+              className={classNames(
+                "flex justify-between items-center p-2 border-0 border-b border-solid border-neutral-300",
+                "hover:bg-neutral-200 cursor-pointer select-none"
+              )}
+            >
+              <div>
+                <div className="flex gap-x-2">
+                  <Typography.Link>{noti.title}</Typography.Link>
+                  <span>-</span>
+                  <Typography.Text className="text-neutral-400">
+                    {dayjs(noti.createAt).fromNow()}
+                  </Typography.Text>
+                </div>
+                <Typography.Text>{noti.description}</Typography.Text>
               </div>
-            }
-            description={dayjs(item.createAt).add(7, "hours").fromNow()}
-          />
-        </List.Item>
-      )}
-    />
+              {!noti.isRead && (
+                <div>
+                  <CircleIcon style={{ fontSize: "12px" }} />
+                </div>
+              )}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
